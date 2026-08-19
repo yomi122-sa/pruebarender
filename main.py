a0a5283795aa02c1cc4b917ef9abdb9a024ace43
yomi122-sa <samuelcortesbv@gmail.com>
2026-08-19 14:39:32 -0600

    gemini model 3.5lite

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
 # --- Clientes de Servicios ---
 genai.configure(api_key=GEMINI_API_KEY)
-gemini_model = genai.GenerativeModel("gemini-2.5-flash-lite")
+gemini_model = genai.GenerativeModel("gemini-3.5-flash-lite")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI(title="Asistente Personal IA")
